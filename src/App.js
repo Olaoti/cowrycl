d475655570e640cbe0223ba8bc0656aea61b8249
Omotola Olaoti <omotolaolaoti@gmail.com>
2023-02-01 22:09:43 +0100

    New

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
+import Navbar from "./Navbar";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App" >
+      <Navbar/>
       <Header />
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 function Header() {
   return (
     <div className="header">
-      <Navbar/>
     </div>
   );
 }
